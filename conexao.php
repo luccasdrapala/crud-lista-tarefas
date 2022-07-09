b000845eeefbb1e1389c2e504384a68d5d9e7fad
luccasdrapala <luccasdrapala@gmail.com>
2022-07-09 11:55:50 -0300

    criando objetos e alguns metodos

---
+++ conexao.php
@@ -0,0 +1,31 @@
+<?php
+
+    class Conexao {
+
+        private $host = 'localhost';
+        private $dbname = 'php_com_pdo';
+        private $user = 'root';
+        private $pass = '';
+
+        public function conectar() {
+            
+            try {
+
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+            } catch(PDOException $erro) {
+
+                echo '<p>' . $erro->getMessage() . '</p>';
+            }
+
+        }
+
+    }
+
+?>
